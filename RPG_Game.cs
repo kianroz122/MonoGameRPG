3b8cd87dceb609a150859db7333e711567d87558
kianroz122 <50154724+kianroz122@users.noreply.github.com>
2021-04-04 12:21:05 +0100

    more work

--- RPG_Game.cs
+++ RPG_Game.cs
@@ -16,7 +16,7 @@ namespace App05MonoGame
     /// random coins and the enemy tries to catch the player.
     /// </summary>
     /// <authors>
-    /// Derek Peacock & Andrei Cruceru
+    /// Kian Rozblat
     /// </authors>
     public class RPG_Game : Game
     {
